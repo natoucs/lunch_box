52dd0fa476c0f2de9eb409e1ef0b27d1d3a5edeb
moshe melmaed <moshe.melamed@mail.huji.ac.il>
2019-01-01 22:11:32 +0200

    server skelaton

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from bottle import (get, post, request, route, run, static_file, jinja2_view)
+from bottle import (get, post, request, route, run,template, static_file, jinja2_view, redirect)
 import json
 from pymysql import connect, cursors
 from functools import partial
@@ -7,10 +7,14 @@ from functools import partial
 view = partial(jinja2_view, template_lookup=['templates'])
 
 
+def user_loged_in():
+    return ("somthig from cocies")
+
+
 # setting the connection to the DB server
 connection = connect(host='localhost',
                      user='root',
-                     password='hilla',
+                     password='momo',
                      db='store',
                      charset='utf8',
                      cursorclass=cursors.DictCursor)
@@ -43,6 +47,66 @@ def home():
 @view('login.html')
 def login():
     return {}
+
+
+@post('/login')
+@view('login.html')
+def login():
+    user_name = request.forms.get("user_name")
+    #check if user exists
+    return ()
+
+
+@get('/home')
+@view('home.html')#not working!
+def back_home():
+        return ()
+
+
+@get('/Sign_Up')
+@view('login.html')
+def Sign_Up():
+        return ()
+
+
+@post('/Sign_Up')
+@view('login.html')
+def prosses_Sign_Up():
+        return ()
+
+
+@get('/Offer')
+@view('offer.html')
+def login_route():
+        return ()
+
+
+@post('/Offer')
+@view('offer.html')
+def login_route():
+        return ()
+
+
+@get('/My_Acount')
+@view('login.html')
+def login_route():
+        return ()
+
+
+@get('/dishes')
+@view('dishes.html')
+def login():
+        return ()
+
+
+@post('/dishes')
+@view('dishes.html')
+def login():
+        return ()
+
+
+
+
 
 
 def main():
